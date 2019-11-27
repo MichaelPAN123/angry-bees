0dc3fc6ff95737952adcd5080fc3dc6e7cb060f0
Leanna-B <Leanna-B@users.noreply.github.com>
2019-11-27 10:41:12 -0500

    Add files via upload

---
+++ oculus/Assets/bee_script1.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bee_script1 : MonoBehaviour
+{
+
+    //Rigidbody rb;
+    //new Vector3 beemov;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //beemov = 
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position += transform.forward * Time.deltaTime;
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("test");
+        OVRInput.SetControllerVibration(1, 1, OVRInput.Controller.RTouch);
+        OVRInput.SetControllerVibration(1, 1, OVRInput.Controller.LTouch);
+
+
+
+
+    }
+
+
+}
